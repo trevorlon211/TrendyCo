7400aecdd550cc4d848a3c9054bae572ba66590e
trevorlon211 <trevorlon.211@gmail.com>
2017-02-15 12:52:31 -0500

    added email capabilities

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'static_pages/about'
   get 'static_pages/contact'
 
-  
+  post 'static_pages/thank_you'
 
   resources :orders, only: [:index, :show, :create, :destroy]
 
