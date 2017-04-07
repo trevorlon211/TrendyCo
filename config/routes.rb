63ef55b5ca9780c5bc4af3d0e135536a9818a869
trevorlon211 <trevorlon.211@gmail.com>
2017-04-07 00:14:30 -0400

    added post

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
    devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
 
+   resources :posts
    resources :users
    resources :products
    resources :contacts, only: [:new, :create]
-  
+
    root 'static_pages#landing_page'
    get 'static_pages/about'
    get 'static_pages/landing_page'
 
 
    resources :orders, only: [:index, :show, :create, :destroy]
- 
+
 
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170316214729) do
+ActiveRecord::Schema.define(version: 20170406233528) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20170316214729) do
     t.float   "total"
     t.index ["product_id"], name: "index_orders_on_product_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
