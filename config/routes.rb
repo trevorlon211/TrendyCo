6fb61e527b326e42a24209431918332bf1037371
trevorlon211 <trevorlon.211@gmail.com>
2017-04-19 17:37:39 -0400

    comment2

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
    devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
 
+   resources :products do
+     resources :comments
+   end
+   
    resources :posts
    resources :users
-   resources :products
+
    resources :contacts, only: [:new, :create]
 
    root 'static_pages#landing_page'
